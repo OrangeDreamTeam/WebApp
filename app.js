c683e38cfbeab956666339ee7324d945f79897b9
Chris Amavisca <chris.amavisca@gmail.com>
2013-11-09 17:43:33 -0500

    Ok

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 var express = require('express')
   , http = require('http')
   , path = require('path')
-  , mysql = require('node-mysql')
+  , mysql = require('mysql')
   , dashboard = require('./routes/dashboard')
-  , android = require('/routes/android');
+  , android = require('./routes/android');
 
 var app = express();
 
+
 connection = mysql.createConnection({
-  host: 'test',
-  user: 'test',
-  password: 'test'
+  host: 'localhost',
+  user: 'root',
+  database: 'srconn'
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+    app.get('/getRoutes', dashboard.getRoutes);
+    app.get('/getPhones', dashboard.getPhones);
+    app.get('/getActiveRoutes', dashboard.getActiveRoutes);
+    app.get('/getRouteByField', dashboard.getRouteByField);
+    app.post('/createRoute', dashboard.createRoute);
 
-app.get('/getRoutes', dashboard.getRoutes);
-app.get('/getPhones', dashboard.getPhones);
-app.get('/getActiveRoutes', dashboard.getActiveRoutes);
-app.get('/getRouteByField', dashboard.getRoutesByFields);
-app.post('/createRoute', dashboard.createRoute);
+    app.post('/android/initializeGCM', android.initializeGCM);
 
-app.post('/android/initializeGCM', android.initializeGCM);
+    if ('development' == app.get('env')) {
+      app.use(express.errorHandler());
+    }
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log('Express server listening on port ' + app.get('port'));
+    });
+  }
 });
