09bf4e39295d79c3affe8aa717ec2b8cd507b19c
Chris Amavisca <chris.amavisca@gmail.com>
2013-11-10 09:45:42 -0500

    push to master hey yuval

--- routes/android.js
+++ routes/android.js
@@ -5,17 +5,36 @@ exports.initializeGCM = function(req, res) {
   var phoneNumber = connection.escape(req.body['phoneNumber']);
   var phoneName = connection.escape(req.body['phoneName']);
 
-  var insertValues = phoneName + ',' + phoneNumber + ',' + gcmKey;
-
-  var insertQuery = 'INSERT INTO Phone VALUES (' + insertValues + ') ON DUPLICATE KEY UPDATE gcmKey = VALUES(' + gcmKey + '), phoneName = VALUES(' + phoneName + ');';
-
-  connection.query(insertQuery, function(err, rows, fields) {
+  var selectQuery = 'SELECT * FROM Phone WHERE Phone.phoneNumber = ' + phoneNumber + ';';
+  connection.query(selectQuery, function(err, rows, fields) {
     if(err) {
       console.log(err);
-      res.send(500);
     }
     else {
-      res.send(200);
+      if(rows.length === 0) {
+        var insertValues = phoneName + ',' + phoneNumber + ',' + gcmKey;
+        var insertQuery = 'INSERT INTO Phone (phoneName, phoneNumber, gcmKey) VALUES (' + insertValues + ');';
+        connection.query(insertQuery, function(err, rows, fields) {
+          if(err) {
+            console.log(err);
+            res.send(500);
+          }
+          else {
+            res.send(200);
+          }
+        });
+      }
+      else {
+        var updateQuery = 'UPDATE Phone SET phoneName=' + phoneName + ', gcmKey=' + gcmKey + ' WHERE Phone.phoneNumber = ' + phoneNumber + ';';
+        connection.query(updateQuery, function(err) {
+          if(err) {
+            console.log(err);
+          }
+          else {
+            res.send(200);
+          }
+        });
+      }
     }
   });
 }
