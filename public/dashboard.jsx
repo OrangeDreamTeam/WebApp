c87e82110a1c116c18823c9bb20a261414274375
Yuval Dekel <thedekel@gmail.com>
2013-11-09 21:28:50 -0500

    decent html for dashboard mayn display

--- public/dashboard.jsx
+++ public/dashboard.jsx
@@ -8,7 +8,13 @@ var Header = React.createClass({
     var login_indicator;
     if (this.props.user) {
       login_indicator = <div class="login-indicator">
-        <img height={64} width={64} src={this.props.user.image_src} />
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+	 width="46px" height="55px" viewBox="0 0 46 55" enable-background="new 0 0 46 55">
+<g>
+	<circle fill="#028C90" cx="24.213" cy="14.207" r="13.714"/>
+	<path fill="#028C90" d="M46,55v-6.543C46,40.896,42.652,35,37.252,35H11.174C5.773,35,0,40.896,0,48.457V55H46z"/>
+</g>
+</svg>
         <span class="user-name">{this.props.user.name}</span>
       </div>;
     } else {
@@ -17,9 +23,9 @@ var Header = React.createClass({
       </div>;
     }
     var logo_style= {
-      height: "117px",
-      width: "280px",
-      backgroundImage: "url(http://srconn.org/templates/theme430/images/logo.gif)"
+      height: "100px",
+      width: "165px",
+      backgroundImage: "url(/dashlogo.png)"
     };
     return <div class='header'>
       <div class='logo' style={logo_style}></div>
@@ -72,7 +78,7 @@ var JQUERYISDUMB = React.createClass({
   }
 });
 
-var PhonesList = React.createClass({
+var ContactList = React.createClass({
   render: function(){
     var phones = this.props.phones, phoneBoxes = [];
     for (var i = 0; i < phones.length; i++) {
@@ -81,7 +87,7 @@ var PhonesList = React.createClass({
         <p>{phones[i].pnum}</p>
         </div>);
     }
-    return <div class='phones-list'><h3>Phones</h3>{phoneBoxes}</div>;
+    return <div class='contact-list'><h3>ContactList</h3>{phoneBoxes}</div>;
   }
 });
 
@@ -99,8 +105,37 @@ var RoutesList = React.createClass({
   }
 });
 
+var AlertsList = React.createClass({
+  render: function() {
+    var alerts = [], alert;
+    for (var i = 0; i < this.props.alerts.length; i++) {
+      alert = this.props.alerts[i];
+      alerts.push(<div class="alert-box">
+          <div class="alert-time">{alert.time.toLocaleTimeString()}</div>
+          <div class="message">
+            <div class="message-body">
+              {alert.message}
+            </div>
+            <div class="message-recipient">
+              <span class="name">{alert.name}</span>
+              <span class="phone">{alert.phone}</span>
+            </div>
+          </div>
+        </div>);
+    }
+    console.log("alerts",alerts);
+    return <div class="alerts-list">
+      <h3>Recent Alerts</h3>
+      <button>View All</button>
+      <div class="alerts">
+        {alerts}
+      </div>
+    </div>;
+  }
+});
+
 var RouteTracker = React.createClass({
-  render: function(){
+  render: function() {
     var routes = this.props.activeRoutes, routeBoxes = [], routejson;
     for (var i = 0; i < routes.length; i++) {
       routejson = JSON.stringify(routes[i]);
@@ -109,7 +144,7 @@ var RouteTracker = React.createClass({
           <h5 class='route-progress'>{'Stops Visited: '+routes[i].progress+'/'+routes[i].route.stops}</h5>
           <div class='route-actions'>
             <button data-type={'schedule'} data-route={routejson} onClick={this.props.onAction}>Schedule</button>
-            <button data-type={'track'} data-route={routejson} onClick={this.props.onAction}>Track</button>
+            <button data-type={'track'} data-route={routejson} onClick={this.props.onAction}>Map</button>
             <button data-type={'call'} data-route={routejson} onClick={this.props.onAction}>Call</button>
             <button data-type={'alert'} data-route={routejson} onClick={this.props.onAction}>Alert</button>
           </div>
@@ -133,7 +168,14 @@ var Dashboard = React.createClass({
         stops: 3},
       progress: 1}
     ];
-    return {activeRoutes: activeRoutes, selectedPhone: null, selectedRoute: null, phones:[], routes:[], twilio: false};
+    var alerts = [
+      {name: "Jamie Crabb",
+       phone: "912-337-1703",
+       time: new Date(2013, 11, 9, 6, 0),
+       message: "Yuval Dekel will not be home today, you need to reschedule for another day."
+      }
+    ];
+    return {activeRoutes: activeRoutes, selectedPhone: null, selectedRoute: null, phones:[], routes:[], alerts: alerts, twilio: false};
   },
   componentWillMount: function() {
     var setState = this.setState;
@@ -197,8 +239,9 @@ var Dashboard = React.createClass({
       <Header served={"100,000"} downloads={"42"} user={{name:"Staff Member", image_src: "http://www.codinghorror.com/.a/6a0120a85dcdae970b017742d249d5970d-800wi"}} />
       <JQUERYISDUMB />
       <RouteTracker onAction={this.handleRouteAction} onMoreInfo={this.showRouteinfo} activeRoutes={this.state.activeRoutes} twilio={this.state.twilio}/>
-      <PhonesList ponSelect={this.selectPhone} phones={this.state.phones} />
       <RoutesList onSelect={this.selectRoute} routes={this.state.routes} />
+      <AlertsList alerts={this.state.alerts} />
+      <ContactList ponSelect={this.selectPhone} phones={this.state.phones} />
     </div>;
   }
 });
